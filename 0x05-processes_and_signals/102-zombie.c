d775ea08f1e8cc2654ed7bacd00daffb4a9b04b9
THOSSYN <mtafortunefinder@gmail.com>
2023-07-28 14:34:32 -0700

    Corrected: 102-zombie.c

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -4,14 +4,26 @@
 #include <fcntl.h>
 #include <sys/types.h>
 
+/**
+ * infinite_while - runs an infinite loop
+ *
+ * Return: nothing
+ */
+
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
+
+/**
+ * main - control start from here and forking
+ *
+ * Return: nothing
+ */
 
 int main(void)
 {
@@ -20,19 +32,19 @@ int main(void)
 
 	for (i = 0; i < 5; i++)
 	{
-	pid = fork();
-	if (pid == -1)
-	{
-	printf("Could not fork a child process\n");
-	exit (0);
-	}
-	if (pid == 0)
-	{
-	child_pid = getpid();
-	printf("Zombie process created, PID: %i\n", child_pid);
-	infinite_while();
-	exit (0);
-	}
+		pid = fork();
+		if (pid == -1)
+		{
+			printf("Could not fork a child process\n");
+			exit(0);
+		}
+		if (pid == 0)
+		{
+			child_pid = getpid();
+			printf("Zombie process created, PID: %i\n", child_pid);
+			infinite_while();
+			/*exit (0);*/
+		}
 	}
 	sleep(2);
 	return (0);
