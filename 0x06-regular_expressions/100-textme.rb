c2158d8809bf48dfacabfb50f00113940e27dc2f
THOSSYN <mtafortunefinder@gmail.com>
2023-08-01 16:03:11 -0700

    Updated: 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 #Matches phone nos of sender and receiver
 
-sender = ARGV[0].scan(/from:(\+?\w*\s*\w*)/).join
-recipient = ARGV[0].scan(/to:(\+?\d+)/).join
-flag= ARGV[0].scan(/flags:((:?-?\d){4})/).join
+sender = ARGV[0].scan(/\[from:(.*?)\]/).join
+recipient= ARGV[0].scan(/\[to:(.*?)\]/).join
+flag= ARGV[0].scan(/\[flags:(.*?)\]/).join
 
 puts "#{sender},#{recipient},#{flag}"
