455bc9c23d5257cd83f1b605eaa4f80fb1ef44df
THOSSYN <mtafortunefinder@gmail.com>
2023-08-01 15:54:09 -0700

    Updated: 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,21 +1,8 @@
 #!/usr/bin/env ruby
+#Matches phone nos of sender and receiver
 
-def extract_info(input)
-  matches = input.scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/)
-  matches.empty? ? nil : matches
-end
+sender = ARGV[0].scan(/from:(\+?\w*\s*\w*)/).join
+recipient = ARGV[0].scan(/to:(\+?\d+)/).join
+flag= ARGV[0].scan(/flags:((:?-?\d){4})/).join
 
-def print_statistics(matches)
-  if matches
-    matches.each do |sender, receiver, flags|
-      puts "#{sender},#{receiver},#{flags}"
-    end
-  else
-    puts "No matches found."
-  end
-end
-
-# Extract sender, receiver, and flags
-input_string = ARGV[0]
-matches = extract_info(input_string)
-print_statistics(matches)
+puts "#{sender},#{recipient},#{flag}"
