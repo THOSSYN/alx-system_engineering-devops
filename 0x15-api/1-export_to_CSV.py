469031210f49dac1a96d1e2132f4f36191b83c2d
THOSSYN <mtafortunefinder@gmail.com>
2023-10-23 14:17:30 -0700

    Added: 1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""A script that returns todos of employees bt id"""
+
+import csv
+import json
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    userid = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com'
+    res = requests.get(f'{url}/todos?userId={userid}')
+    res1 = requests.get(f'{url}/users/{userid}')
+
+    res1_json = res1.json()
+    name = res1_json.get('name')
+
+    to_json = res.json()
+
+    with open(f'{userid}.csv', "w", newline='') as fptr:
+        content = csv.writer(fptr, quoting=csv.QUOTE_ALL)
+        for item in to_json:
+            content.writerow([
+                item["userId"],
+                f'{name}',
+                item["completed"],
+                item["title"]
+            ])
